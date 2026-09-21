fbae953401ac5d93bdd64e0a735c8568de326c53
alexseuf <48165001+alexseuf@users.noreply.github.com>
2026-09-21 20:18:17 +0200

    Update data model test for new overview signals

--- raspberry-pi/tests/test_data_model.py
+++ raspberry-pi/tests/test_data_model.py
@@ -9,7 +9,7 @@ class DataModelTest(unittest.TestCase):
         with open('config/topics.yaml', encoding='utf-8') as handle:
             cls.defs=yaml.safe_load(handle)['signals']
     def test_all_required_signals_present(self):
-        required={'grid_voltage_l1','grid_voltage_l2','grid_voltage_l3','grid_current_l1','grid_current_l2','grid_current_l3','grid_power_l1','grid_power_l2','grid_power_l3','grid_frequency','input_power','dc_link_voltage','output_voltage','output_current','output_power','temp_pfc','temp_dab_primary','temp_dab_secondary','temp_inductor','temp_transformer'}
+        required={'grid_voltage_l1','grid_voltage_l2','grid_voltage_l3','grid_current_l1','grid_current_l2','grid_current_l3','grid_power_l1','grid_power_l2','grid_power_l3','grid_frequency','input_power','dc_link_voltage','output_voltage','output_current','output_power','earth_voltage','temp_pfc','temp_dab_primary','temp_dab_secondary','temp_pcb_primary','temp_pcb_secondary','temp_inductor','temp_transformer'}
         self.assertEqual(required,set(self.defs))
     def test_valid_invalid_and_stale(self):
         m=DataModel(self.defs,1);topic=self.defs['output_voltage']['topic']
